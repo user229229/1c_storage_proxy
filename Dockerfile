60e2de5e711b7d8b1df109067a86b3783ae6c137
Andrey Shorikov <shorikov@syspod.ru>
2023-04-11 21:13:53 +0300

    First version

---
+++ Dockerfile
@@ -0,0 +1,37 @@
+# Сервер хранилища 1С 8.3
+#
+FROM debian:stretch-slim
+
+ENV DIST server64_8_3_22_1750
+
+
+RUN apt-get update && apt-get install -y \
+	wget \
+	&& rm -rf /var/lob/apt/lists/*
+
+RUN apt-get install apache2 -y
+COPY ./httpd.conf /etc/apache2/sites-available/1c_httpd.conf
+COPY ./1crep.1ccr /var/www/crs/1crep.1ccr
+
+ENV SRV1CV8_REPOSITORY /opt/1C/repository
+
+RUN wget http://casa.ru/${DIST}.tar.gz -P /tmp --no-check-certificate | wc -l > /number
+
+
+RUN tar xzf /tmp/${DIST}.tar.gz -C /tmp && \
+	/tmp/setup-full-*-x86_64.run --mode unattended --enable-components config_storage_server,ru && \
+	mkdir -p /var/log/1c/dumps && chmod -R 777 /var/log/1c && \
+	rm -rf /tmp/* && \
+	rm -rf /var/lib/apt/lists/*
+
+
+COPY run.sh /
+RUN chmod +x /run.sh
+RUN mkdir -p ${SRV1CV8_REPOSITORY}
+RUN chmod 777 ${SRV1CV8_REPOSITORY}
+
+
+VOLUME ${SRV1CV8_REPOSITORY}
+
+EXPOSE 1542
+CMD ["/run.sh"]
---
+++ storage-proxy.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+# This is a simple port-forward / proxy, written using only the default python
+# library. If you want to make a suggestion or fix something you can contact-me
+# at voorloop_at_gmail.com
+# Distributed over IDC(I Don't Care) license
+import socket
+import select
+import time
+import sys
+import re
+
+# Changing the buffer_size and delay, you can improve the speed and bandwidth.
+# But when buffer get to high or delay go too down, you can broke things
+buffer_size = 1048576
+delay = 0.0001
+forward_to = ('127.0.0.1', 80)
+#forward_to = ('storage.syspod.ru', 8080)
+
+class Forward:
+    def __init__(self):
+        self.forward = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def start(self, host, port):
+        try:
+            self.forward.connect((host, port))
+            return self.forward
+        except Exception as e:
+            print (e)
+            return False
+
+class TheServer:
+    input_list = []
+    channel = {}
+
+    def __init__(self, host, port):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server.bind((host, port))
+        self.server.listen(200)
+
+    def main_loop(self):
+        self.input_list.append(self.server)
+        while 1:
+            time.sleep(delay)
+            ss = select.select
+            inputready, outputready, exceptready = ss(self.input_list, [], [])
+            for self.s in inputready:
+                if self.s == self.server:
+                    self.on_accept()
+                    break
+
+                self.data = self.s.recv(buffer_size)
+                if len(self.data) == 0:
+                    self.on_close()
+                    break
+                else:
+                    self.on_recv()
+
+    def on_accept(self):
+        forward = Forward().start(forward_to[0], forward_to[1])
+        clientsock, clientaddr = self.server.accept()
+        if forward:
+            print (clientaddr, "has connected")
+            self.input_list.append(clientsock)
+            self.input_list.append(forward)
+            self.channel[clientsock] = forward
+            self.channel[forward] = clientsock
+        else:
+            print ("Can't establish connection with remote server.",)
+            print ("Closing connection with client side", clientaddr)
+            clientsock.close()
+
+    def on_close(self):
+        print (self.s.getpeername(), "has disconnected")
+        #remove objects from input_list
+        self.input_list.remove(self.s)
+        self.input_list.remove(self.channel[self.s])
+        out = self.channel[self.s]
+        # close the connection with client
+        self.channel[out].close()  # equivalent to do self.s.close()
+        # close the connection with remote server
+        self.channel[self.s].close()
+        # delete both objects from channel dict
+        del self.channel[out]
+        del self.channel[self.s]
+
+    def on_recv(self):
+        data = self.data
+        # here we can parse and/or modify the data before send forward	    
+        data2 = re.sub(b'version="8\.3\.\d\d\.\d\d\d\d"', b'version="8.3.22.1750"',  data, 1)
+        #data2 = re.sub(b'8.3.20.1789', b'8.3.15.1830',  data, 1)
+        print ("Data length", len(data2))
+        self.channel[self.s].send(data2)
+
+if __name__ == '__main__':
+    port = 8899
+    server = TheServer('localhost', port)
+    try:
+        print ("Server running on port", port)
+        server.main_loop()
+    except KeyboardInterrupt:
+        print ("Ctrl C - Stopping server")
+        sys.exit(1)
