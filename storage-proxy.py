e469d0c1c2f9d88e7c793ebde10333623cc42d0a
Andrey Shorikov <shorikov@syspod.ru>
2023-04-12 20:45:24 +0300

    1

--- storage-proxy.py
+++ storage-proxy.py
@@ -94,7 +94,7 @@ class TheServer:
 
 if __name__ == '__main__':
     port = 8899
-    server = TheServer('localhost', port)
+    server = TheServer('mail.syspod.ru', port)
     try:
         print ("Server running on port", port)
         server.main_loop()
